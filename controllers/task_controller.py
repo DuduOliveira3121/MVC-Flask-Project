5681c2d55bdbe22fe03bb43cc94fd7b3c25d116e
Eduardo Oliveira <duduoliveiras3121@gmail.com>
2025-09-07 19:21:52 -0300

    Add files via upload

---
+++ controllers/task_controller.py
@@ -0,0 +1,46 @@
+from flask import render_template, request, redirect, url_for
+from models.user import db
+from models.task import Task
+from models.user import ser
+
+class TaskController:
+
+    @staticmethod
+    def list_tasks():
+        tasks = Task.query.all()
+        return render_template("tasks.html", tasks=tasks)
+
+    @staticmethod
+    def create_task():
+        if request.method == "POST":
+            title = request.form["title"]
+            description = request.form["description"]
+            user_id = request.form["user_id"]
+
+            Obj_Task = Task(title = title, description = description, user_id = user_id, status = "Pendente")
+            db.session.add(Obj_Task)
+            db.session.commit()
+
+            return redirect(url_for("list_tasks"))
+
+        users = ser.query.all()
+        return render_template("create_task.html", users=users)
+    
+    @staticmethod
+    def update_task_status(task_id):
+        task = Task.query.get(task_id)
+        if task:
+            task.status = "Concluído" if task.status == "Pendente" else "Pendente"
+            db.session.commit()
+
+        return redirect(url_for("list_tasks"))
+
+    @staticmethod
+    def delete_task(task_id):
+        
+        task = Task.query.get(task_id)
+        if task:
+            db.session.delete(task)
+            db.session.commit() 
+    
+        return redirect(url_for("list_tasks"))
